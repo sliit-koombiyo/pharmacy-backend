52fa10f2afa2656e895cc7df8c85f18f3dfc864e
dammakaru <dammakaru@gmail.com>
2018-06-12 06:41:44 +0530

    testing git

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000
 app.use(bodyParser.json());
 app.use(cors());
 app.use('/', (req, res)  => {
-    res.status(404).send("Sorry we couldn't find that page");
+    res.status(404).send("Sorry Koombiyo-Pharmacy couldn't find that page");
 })
 
 app.listen(PORT, (err) => {
