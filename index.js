a0c8097c9996724e13f9a9891dce26aaa978efe2
dammakaru <dammakaru@gmail.com>
2018-06-12 06:32:25 +0530

    initial setup

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = process.env.PORT || 5000
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use('/', (req, res)  => {
+    res.status(404).send("Sorry we couldn't find that page");
+})
+
+app.listen(PORT, (err) => {
+    if(err) {
+        process.exit(-1);
+    }
+
+    console.log('Pharmacy Server started on port ' + PORT);
+})
