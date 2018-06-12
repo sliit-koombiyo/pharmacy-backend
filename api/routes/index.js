79efd4bbd1f534c0ab4cae6881f8971c25906ae9
udrathnayaka <rathnayakar07@gmail.com>
2018-06-12 13:26:30 -0700

    test

--- api/routes/index.js
+++ api/routes/index.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 
 // default route
 router.use('/', (req, res)  => {
-    res.status(404).send("Sorry Koombiyo-Pharmacy couldn't find that page");
+    res.status(404).send("Sorry Koombiyo-Pharmacy couldn't find that page !");
 });
 
 module.exports = router;
