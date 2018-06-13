b8aa71a2254cdfdc825d078344b5cc60164e3324
dammakaru <dammakaru@gmail.com>
2018-06-13 11:13:34 +0530

    added a stock router

--- api/routes/index.js
+++ api/routes/index.js
@@ -4,6 +4,7 @@ const drugs = require('./drugs');
 
 router.use('/prescriptions', prescriptions);
 router.use('/drugs', drugs);
+router.use('/stocks', stocks);
 // default route
 router.use('/', (req, res)  => {
     res.status(404).send("Sorry Koombiyo-Pharmacy couldn't find that page !");
