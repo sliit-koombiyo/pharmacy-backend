1ef052e84e1cc0c1f3714320a550d078f39f1d53
dammakaru <dammakaru@gmail.com>
2018-06-14 23:37:44 +0530

    dispense functionality

--- api/controllers/prescriptionController.js
+++ api/controllers/prescriptionController.js
@@ -1,4 +1,5 @@
 const Prescription = require('../models/Prescription');
+const Drug = require('../models/Drug');
 
 module.exports.addPrescription = (prescription) => {
     return Prescription.create(prescription);
@@ -18,21 +19,21 @@ module.exports.getPrescriptionsByPatientID = (id) => {
 
 module.exports.dispense = (prescription) => {
     console.log("dispensed :" + prescription._id);
-    var criteria = {
-        _id:{ $in: request.payload.split(',')}
-       };
     return new Promise((resolve, reject)=>{
-        Prescription.update(criteria, { status: true }, { multi: true }, (err, res) => {
-            if(err) { 
-                callback(err, null);
-            } else { 
-                callback(null, res);
-            }
-         } )
         if(prescription.prescriptionItems){
-            resolve(prescription.prescriptionItems);
+            prescription.prescriptionItems.forEach((item)=>{
+                const negative = item.quantity * -1;
+                console.log(item.drug);
+                Drug.findOneAndUpdate({"name": item.drug}, {"$inc": {"stock":  negative}}).then((result)=>{
+                    // TODO: add handler for successful update
+                }).catch((err)=>{
+                    reject(err);
+                });
+            });
+
+            resolve("result OK");
         } else {
-            reject(new Error('error'));
+            reject(new Error('error: could not find prescription items'));
         }
     })
     
