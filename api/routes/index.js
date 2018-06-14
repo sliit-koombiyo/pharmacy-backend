9f7d59ad6a5f504843e4d7b030854fde190d5bc7
Reeshma Hassen <hassenreeshma@gmail.com>
2018-06-14 14:44:27 +0530

    Added routes to index for drug batch

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const prescriptions = require('./prescriptions');
 const drugs = require('./drugs');
+const drugBatch = require('./drugBatch');
 
 router.use('/prescriptions', prescriptions);
 router.use('/drugs', drugs);
+router.use('/drugsBatch',drugBatch);
 // default route
 router.use('/', (req, res)  => {
     res.status(404).send("Sorry Koombiyo-Pharmacy couldn't find that page !");
