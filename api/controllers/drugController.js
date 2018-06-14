a51fe2845de58dec36ed4d4e545e4d22fdf87aff
dammakaru <dammakaru@gmail.com>
2018-06-14 14:40:50 +0530

    fixed route error

--- api/controllers/drugController.js
+++ api/controllers/drugController.js
@@ -1,60 +1,62 @@
 const DrugModal = require('../models/Drug');
 
-var DrugController = function(){
-    this.addDrug = (data)=>{
-        return new Promise((resolve,reject)=>{
-            var drug= new DrugModal({
-                drugID:data.drugID,
-                supplier:data.supplier,
-                name:data.name,
-                stock:data.stock,
-                type:data.type,
-                price:data.price,
-                dangerlevel:data.dangerlevel,
-                reorderLevel:data.reorderLevel
+var DrugController = function () {
+    this.addDrug = (data) => {
+        return new Promise((resolve, reject) => {
+            var drug = new DrugModal({
+                drugID: data.drugID,
+                supplier: data.supplier,
+                name: data.name,
+                stock: data.stock,
+                type: data.type,
+                price: data.price,
+                dangerlevel: data.dangerlevel,
+                reorderLevel: data.reorderLevel
             });
-            drug.save().then(()=>{
-                resolve({status: 200, message: "Succesfully added new drug"});
+            drug.save().then(() => {
+                resolve({ status: 200, message: "Succesfully added new drug" });
             }).catch(err => {
-                reject({status: 500, message: "Error:- "+err});
+                reject({ status: 500, message: "Error:- " + err });
             })
         });
     }
 
-    this.UpdateDrug = (id,data)=>{
-        return new Promise((resolve,reject)=>{
-                DrugModal.update({_id:id},data).then(()=>{
-                    resolve({status: 200, message: "succesfully updated the drug"});
-                }).catch(err => {
-                    reject({status: 500, message: "Error:- " + err});
-                });
+    this.UpdateDrug = (id, data) => {
+        return new Promise((resolve, reject) => {
+            DrugModal.update({ _id: id }, data).then(() => {
+                resolve({ status: 200, message: "succesfully updated the drug" });
+            }).catch(err => {
+                reject({ status: 500, message: "Error:- " + err });
+            });
         });
     }
 
-    this.GetAllDrugs = ()=>{
-        DrugModal.find().exec().then((drugs)=>{
-            resolve({status: 200, data: drugs});
-        }).catch(err => {
-            reject({status: 500, message: "Error:- " + err});
-        })
-    }
-
-    this.findADrug = (id)=>{
+    this.GetAllDrugs = () => {
         return new Promise((resolve, reject) => {
-            DrugModal.find({_id:id}).exec().then((drug)=>{
-                resolve({status: 200, data: drug});
+            DrugModal.find({}).exec().then((drugs) => {
+                resolve({ status: 200, data: drugs });
             }).catch(err => {
-                reject({status: 500, message: "Error:- " + err});
+                reject({ status: 500, message: "Error:- " + err });
             })
         })
     }
 
-    this.DeleteADrug = (id)=>{
+    this.findADrug = (id) => {
         return new Promise((resolve, reject) => {
-            DrugModal.remove({_id:id}).then(()=>{
-                resolve({status: 200, message: "Successfullt deleted the drug"});
+            DrugModal.find({ _id: id }).exec().then((drug) => {
+                resolve({ status: 200, data: drug });
             }).catch(err => {
-                reject({status: 500, message:"Error:- " + err});
+                reject({ status: 500, message: "Error:- " + err });
+            })
+        })
+    }
+
+    this.DeleteADrug = (id) => {
+        return new Promise((resolve, reject) => {
+            DrugModal.remove({ _id: id }).then(() => {
+                resolve({ status: 200, message: "Successfullt deleted the drug" });
+            }).catch(err => {
+                reject({ status: 500, message: "Error:- " + err });
             });
         });
     }
